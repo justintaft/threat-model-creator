78cf90c6497e8aa130ff9a31bd113e55acaa78ff
justin taft <justintaft@users.noreply.github.com>
2020-04-18 01:54:15 -0400

    Add code to render user process elements

--- src/cljs/threatmodeler/core.cljs
+++ src/cljs/threatmodeler/core.cljs
@@ -10,6 +10,7 @@
 (defonce time-updater (js/setInterval
                        #(reset! timer (js/Date.)) 1000))
 
+
 (defn greeting [message]
   [:h1 message])
 
@@ -26,13 +27,28 @@
             :value @time-color
             :on-change #(reset! time-color (-> % .-target .-value))}]])
 
-(defn simple-example []
+
+(defn render-threat-model-element [element]
+  [:span {:class (str "diagram-" (name (:type element)))
+          :style {:left (:x element) :top (:y element)}} "hmm"])
+
+
+
+(defn simple-example [threat-model]
   [:div
-   [:span.circle "test"]
+   (for [element (:elements threat-model)]
+     (render-threat-model-element element))
    [greeting "Hello world, it is now"]
    [clock]
    [color-input]])
 
-(defn ^:export init! []
-  (rdom/render [simple-example] (js/document.getElementById "app")))
+(def threat-model { :elements [ {:type :actor :name "hackerman" :id "1" :x 30 :y 30}
+                                {:type :process :name "webapp" :id "2" :x 30 :y 80}]
+                     :threats []})
+
+
+
+
+(defn ^:export init! [])
+(rdom/render [simple-example threat-model] (js/document.getElementById "app"))
 
