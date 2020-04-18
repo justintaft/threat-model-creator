6e9fb0ebbc408942e16edb50d04d4101d37c3a0b
justin taft <justintaft@users.noreply.github.com>
2020-04-18 14:21:01 -0400

    Adds drawoing of lines between diagram elements

--- src/cljs/threatmodeler/core.cljs
+++ src/cljs/threatmodeler/core.cljs
@@ -38,41 +38,52 @@
         slope (/ (- y2 y1) (- x2 x1))
         rotationDegree (js/Math.atan slope)]
 
-    [:div {:style {:display :inline-block
-                   :height "1px"
-                   :width (goog.string.format "%dpx" lineLength)
-                   :color "black"
-                   :background-color "black"
-                   :transform (goog.string.format "rotate(%frad)" rotationDegree )
-                   :transform-origin "center left"}}]))
+    [:div.line.diagram-threat-model-element {:style {:display :inline-block
+                                                     :left "0px"
+                                                     :top "0px"
+                                                     :height "2px"
+                                                     :width (goog.string.format "%dpx" lineLength)
+                                                     :color "black"
+                                                     :background-color "black"
+                                                     :transform (goog.string.format "translate(%dpx,%dpx) rotate(%frad)" x1 y1 rotationDegree )
+                                                     :transform-origin "center left"}}]))
+
+(defn calculate-line-points [element1 element2]
+  (let [ele1WidthDiv2 (/ (:width element1) 2)
+        ele1HeightDiv2 (/ (:height element1) 2)
+        ele2WidthDiv2 (/ (:width element2) 2)
+        ele2HeightDiv2 (/ (:height element2) 2)]
+    {:x1 (+ (:x element1) (:width element1) )
+     :y1 (+ (:y element1) ele1WidthDiv2)
+     :x2 (:x element2)
+     :y2 (+ (:y element2) ele2HeightDiv2)}))
 
 
+(defn render-threat-model-element-common [{:keys [x y type]}]
+  [draggable {:grid [25 25] :defaultPosition {:x x :y y}}
+   [:span {:class (str "diagram-" (name type))
+           :style {:transform (goog.string.format "translate(%dpx,%dpx)" x y)}}]])
 
-(defn render-threat-model-element-common [element]
-  [draggable {:grid [25 25]}
-   [:span {:class (str "diagram-" (name (:type element)))
-           :style {:left (:x element) :top (:y element)}} "hmm"]])
 
-(defn render-threat-model-element-communication [element]
-  [render-line {:x1 0 :y1 0 :x2 200 :y2 200}])
-  
+(defn render-threat-model-element-communication [element elements]
+  [render-line (calculate-line-points ((:from element) elements) ((:to element) elements))])
 
-(defmulti render-threat-model-element :type)
+(defmulti render-threat-model-element (fn [element _] (:type element)))
 (defmethod render-threat-model-element :actor [element] (render-threat-model-element-common element))
 (defmethod render-threat-model-element :process [element] (render-threat-model-element-common element))
-(defmethod render-threat-model-element :communication [element] (render-threat-model-element-communication element))
+(defmethod render-threat-model-element :communication [element elements] (render-threat-model-element-communication element elements))
 
 (defn simple-example [threat-model]
 [:div
- (for [element (:elements threat-model)]
-   (render-threat-model-element element))
- [greeting "Hello world, it is now"]
- [clock]
- [color-input]])
+(for [element (vals (:elements threat-model))]
+  (render-threat-model-element element (:elements threat-model)))
+[greeting "Hello world, it is now"]
+[clock]
+[color-input]])
 
-(def threat-model { :elements [ {:type :actor :name "hackerman" :id "1" :x 30 :y 30}
-                               {:type :process :name "webapp" :id "2" :x 30 :y 80}
-                               {:type :communication :from "1" :to "2"}]
+(def threat-model { :elements { :1 {:type :actor :name "hackerman" :x 30 :y 30 :width 100 :height 100}
+                               :2 {:type :process :name "webapp" :id "2" :x 400 :y 80 :width 100 :height 50}
+                               :3 {:type :communication :from :1 :to :2 }}
                    :threats []})
 
 
