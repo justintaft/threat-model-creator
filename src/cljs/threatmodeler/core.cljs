0dd412cd0011f3691f70518aa58fb06cfec6b4bb
justin taft <justintaft@users.noreply.github.com>
2020-04-18 13:11:05 -0400

    Add code to draw line

--- src/cljs/threatmodeler/core.cljs
+++ src/cljs/threatmodeler/core.cljs
@@ -2,7 +2,8 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
             [clojure.string :as str]
-            [cljsjs.react-draggable]))
+            [cljsjs.react-draggable]
+            [goog.string.format]))
 
 (defonce timer (r/atom (js/Date.)))
 
@@ -28,17 +29,33 @@
             :value @time-color
             :on-change #(reset! time-color (-> % .-target .-value))}]])
 
-
 (def draggable (r/adapt-react-class js/ReactDraggable))
 
+
+(defn render-line [ {:keys [x1 y1 x2 y2] } ]
+  (let [lineLength (js/Math.sqrt (+ (js/Math.pow (- x2 x1) 2)
+                                    (js/Math.pow (- y2 y1) 2)))
+        slope (/ (- y2 y1) (- x2 x1))
+        rotationDegree (js/Math.atan slope)]
+
+    [:div {:style {:display :inline-block
+                   :height "1px"
+                   :width (goog.string.format "%dpx" lineLength)
+                   :color "black"
+                   :background-color "black"
+                   :transform (goog.string.format "rotate(%frad)" rotationDegree )
+                   :transform-origin "center left"}}]))
+  
+                          
 (defn render-threat-model-element [element]
   [draggable {:grid [25 25]}
-    [:span {:class (str "diagram-" (name (:type element)))
-            :style {:left (:x element) :top (:y element)}} "hmm"]])
+   [:span {:class (str "diagram-" (name (:type element)))
+           :style {:left (:x element) :top (:y element)}} "hmm"]])
 
 
 (defn simple-example [threat-model]
   [:div
+   [render-line {:x1 0 :y1 0 :x2 200 :y2 200}]
    (for [element (:elements threat-model)]
      (render-threat-model-element element))
    [greeting "Hello world, it is now"]
