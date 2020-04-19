21e7e1cac2b76895324ed645c71603f31db39ea0
justin taft <justintaft@users.noreply.github.com>
2020-04-19 11:22:27 -0400

    Fix lines pointing in wrong directino when rendered from top and bottom of elements

--- src/cljs/threatmodeler/core.cljs
+++ src/cljs/threatmodeler/core.cljs
@@ -3,13 +3,15 @@
             [reagent.dom :as rdom]
             [clojure.string :as str]
             [cljsjs.react-draggable]
-            [goog.string.format]))
+            [goog.string.format]
+            [cljs.test :refer-macros [deftest is testing]]))
 
 (def threat-model (r/atom { :elements { 1 {:type :actor :name "hackerman" :x 50 :y 50 :width 100 :height 50 :id 1}
                                        2 {:type :process :name "webapp" :id 2 :x 400 :y 100 :width 100 :height 100}
                                        3 {:type :datastore :name "datastore" :id 3 :x 100 :y 400 :width 100 :height 50}
-                                       4 {:type :communication :from 1 :to 2 :what "cool"}
-                                       5 {:type :boundary :x1 100 :y1 200 :x2 100 :y2 300}}
+                                       4 {:type :communication :from 1 :to 2}
+                                       5 {:type :communication :from 1 :to 3}
+                                       6 {:type :boundary :x1 100 :y1 200 :x2 100 :y2 300}}
                            :threats []}))
 
 
@@ -36,38 +38,75 @@
 
 (def draggable (r/adapt-react-class js/ReactDraggable))
 
-
 (defn calculate-line-points [element1 element2]
-  (let [[leftEle rightEle] (if (< (:x element1) (+ (:x element2) (:width element2)))
-                             [element1 element2]
-                             [element2 element1])
-        leftEleWidthDiv2 (/ (:width leftEle) 2)
-        leftEleHeightDiv2 (/ (:height leftEle) 2)
-        rightEleWidthDiv2 (/ (:width rightEle) 2)
-        rightEleHeightDiv2 (/ (:height rightEle) 2)]
+  "Find best line between two elements, and return points."
 
-    {:x1 (+ (:x leftEle) (:width leftEle))
-     :y1 (+ (:y leftEle) leftEleHeightDiv2)
-     :x2 (:x rightEle)
-     :y2 (+ (:y rightEle) rightEleHeightDiv2)}))
+  (cond
+    ;left of second element
+    (< (+ (:x element1) (:width element1)) (:x element2))
+    {:x1 (+ (:x element1) (:width element1))
+     :y1 (+ (:y element1) (/ (:height element1) 2))
+     :x2 (:x element2)
+     :y2 (+ (:y element2) (/ (:height element2) 2))}
+
+    ;to right of second element
+    (> (:x element1) (+ (:x element2) (:width element2)))
+    {:x1 (+ (:x element2) (:width element2))
+     :y1 (+ (:y element2) (/ (:height element2) 2))
+     :x2 (:x element1)
+     :y2 (+ (:y element1) (/ (:height element1) 2))}
+
+
+    ;above second element
+    (< (:y element1) (:y element2))
+    {:x1 (+ (:x element1) (/ (:width element1) 2))
+     :y1 (+ (:y element1) (:height element1))
+     :x2 (+ (:x element2) (/ (:width element2) 2))
+     :y2 (:y element2)}
+
+
+    ;below second elements
+    true
+    {:x1 (+ (:x element2) (/ (:width element2) 2))
+     :y1 (+ (:y element2) (:height element2))
+     :x2 (+ (:x element1) (/ (:width element1) 2))
+     :y2 (:y element1)}))
+
+    
+
+
+                                            
+
 
 
 (defn render-line [ {:keys [x1 y1 x2 y2 style] :as line}]
   (let [lineLength (js/Math.sqrt (+ (js/Math.pow (- x2 x1) 2)
                                     (js/Math.pow (- y2 y1) 2)))
-        slope (/ (- y2 y1) (- x2 x1))
-        rotationDegree (js/Math.atan slope)
+
+        slope (/ (- y1 y2) (- x1 x2))
+
+
+        ;We have to do some funky math to rotations, as Y is inverted on screen
+        ;(higher Y cord is lowe ron screen...)
+        rotationDegree (if (and (>= x1 x2)
+                                (< y1 y2))
+                         (- js/Math.PI   (* -1 (js/Math.atan slope)))
+                         (js/Math.atan slope))
         style (or style "solid")]
+        
+
+    
+    (js/console.log rotationDegree)
 
     [(if (:draggable line) draggable :span) [:div.line.diagram-threat-model-element {:style {:height "2px"
                                                                                              :width (goog.string.format "%dpx" lineLength)
                                                                                              :border-top (goog.string.format "2px %s black" style)
                                                                                              :transform (goog.string.format "translate(%dpx,%dpx) rotate(%frad)" x1 y1 rotationDegree)
                                                                                              :transform-origin "center left"
-                                                                                             :padding (if (:draggable line) "5px" "0")
+                                                                                             :padding (if (:draggable line) "5px" "0")}}]]))
 
-                                                                                     }}
-     ]]))
+                                                                                     
+     
 
 (defn diagram-event-element-drag-stop [id event data]
   "Persist position of dragged threat model diagram element to local state."
